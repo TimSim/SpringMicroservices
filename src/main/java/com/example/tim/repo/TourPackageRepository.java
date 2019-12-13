1fd5e8a4963ef54b4813add8289c4bc56c5c7565
Tim Simeonov <timsimeonov@gmail.com>
2019-12-12 16:59:19 -0800

    Restricted access to save and delete REST points

--- src/main/java/com/example/tim/repo/TourPackageRepository.java
+++ src/main/java/com/example/tim/repo/TourPackageRepository.java
@@ -3,6 +3,9 @@ package com.example.tim.repo;
 
 import com.example.tim.domain.TourPackage;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.Optional;
 
@@ -11,6 +14,8 @@ import java.util.Optional;
  *
  * Created by Mary Ellen Bowman
  */
+//Control access at class level and overwrite default method name from tourPackages to packages
+@RepositoryRestResource(collectionResourceRel = "packages", path = "packages")
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
 
     /**
@@ -19,5 +24,30 @@ public interface TourPackageRepository extends CrudRepository<TourPackage, Strin
      * @param name name of the package
      * @return Optional of TourPackage
      */
-    Optional<TourPackage> findByName(String name);
+    Optional<TourPackage> findByName(@Param("name")String name);
+
+    //Restrict access at method level - restrict access from outside world / REST to save()
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> S save(S s);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> Iterable<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(String s);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(TourPackage tourPackage);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends TourPackage> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
--- src/main/java/com/example/tim/repo/TourRepository.java
+++ src/main/java/com/example/tim/repo/TourRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 
 /**
@@ -20,4 +21,28 @@ public interface TourRepository extends CrudRepository<Tour, Integer> {
      * @return List of found tours.
      */
     Page<Tour> findByTourPackageCode(@Param("code")String code, Pageable pageable);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> S save(S s);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> Iterable<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Tour tour);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Tour> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
